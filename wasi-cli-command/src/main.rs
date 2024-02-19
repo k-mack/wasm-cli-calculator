0d9139c3caa131434eb647b4a0860ca8d1a121d8
Kevin Macksamie <kevin.macksamie@gmail.com>
2024-02-18 17:57:52 -0800

    feat: add wasi-cli-command error handling

--- wasi-cli-command/src/main.rs
+++ wasi-cli-command/src/main.rs
@@ -1,19 +1,29 @@
 mod bindings;
 
-use crate::bindings::calc::basic::types::Numeric;
 use crate::bindings::calc::basic::calculate::{eval_expression, Op};
+use crate::bindings::calc::basic::types::Numeric;
 
 fn main() {
     let mut args = std::env::args();
 
-    let _ = args.next(); // ignore first arg as it is the program name
+    let Some(app_name) = args.next() else {
+        panic!("Error: program name not available");
+    };
 
-    // TODO: handle erroneous inputs
-    let x = args.next().unwrap().parse::<Numeric>().unwrap();
-    let y = args.next().unwrap().parse::<Numeric>().unwrap();
-    let op = args.next().unwrap();
+    let Some(Ok(x)) = args.next().map(|s| s.parse::<Numeric>()) else {
+        usage(&app_name);
+        return;
+    };
+    let Some(Ok(y)) = args.next().map(|s| s.parse::<Numeric>()) else {
+        usage(&app_name);
+        return;
+    };
+    let Some(op) = args.next() else {
+        usage(&app_name);
+        return;
+    };
 
-    let result = match op.as_str() {
+    let result = match op.to_lowercase().as_str() {
         "add" => Some((eval_expression(Op::Add, x, y), "+")),
         "subtract" => Some((eval_expression(Op::Subtract, x, y), "-")),
         _ => None,
@@ -22,6 +32,10 @@ fn main() {
     if let Some((answer, op_symbol)) = result {
         println!("{} {} {} = {}", x, op_symbol, y, answer);
     } else {
-        println!("unsupported operation \"{}\"", op);
+        usage(&app_name);
     }
+}
+
+fn usage(app_name: &str) {
+    println!("usage: {} <number> <number> <add | subtract>", app_name);
 }
